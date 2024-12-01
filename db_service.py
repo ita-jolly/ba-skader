22a4d97c6914a15992000283c3437818bc3dade6
Josef-TL <j.tonnesenlucas@gmail.com>
2024-12-01 20:12:58 +0100

    create and get functionality implemented

--- db_service.py
+++ db_service.py
@@ -33,3 +33,29 @@ def init():
 
 
         con.commit()
+
+def get_skader():
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM skader')
+
+        rows = cur.fetchall()
+
+        if not rows:
+            return None
+
+        return [{'skade_id': row[0], 'skade_type': row[1], 'skade_pris': row[2], 'nummerplade': row[3], 'syn_type': row[4], 'indberetnings_dato': row[5]} for row in rows]
+
+
+def post_skade(skade_type, skade_pris, nummerplade, syn_type, indberetnings_dato):
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('''
+        INSERT INTO skader (skade_type, skade_pris, nummerplade, syn_type, indberetnings_dato)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (skade_type, skade_pris, nummerplade, syn_type, indberetnings_dato))
+
+        cur.execute('SELECT * FROM skader')
+        last_row = cur.fetchone()
+
+        return {'skade_id': last_row[0], 'skade_type': last_row[1], 'skade_pris': last_row[2], 'nummerplade': last_row[3], 'syn_type': last_row[4], 'indberetnings_dato': last_row[5]}
